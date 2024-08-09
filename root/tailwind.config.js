1faa352f3291092734613012ebcccb6ac2160bf3
Grant Hartman <granthartmandesign@gmail.com>
2024-08-08 17:53:24 -0700

    first commit

---
+++ root/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      'black': '#000',
+      'white': '#fff',
+      'lilac': '#dadbf1',
+      'periwinkle': '#4C5FD5',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
